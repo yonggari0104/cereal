deaf9c2cebc4a73808fc503dcc13887f2ec8f5e4
Jason <jason.y.jeong@gmail.com>
2020-08-24 08:16:26 -0400

    first commit

---
+++ cereal.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+
+cer = pd.read_csv('C:\\Users\\user\\.spyder-py3\\cereal\\cereal.csv')
+
+
+#SEE IF NaN VALUES EXISTS
+print(cer.isna().any())
+
+print(cer.info())
+print(cer.head())
+print(cer.dtypes)
+print(cer.describe())
+
